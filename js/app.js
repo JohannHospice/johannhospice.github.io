a08ebeca37108487f29ef92e61f538ef6b4cc904
Johann Hospice <mhojo972@gmail.com>
2016-07-13 18:51:17 +0200

    Correction of English translation

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 const TEXT = {
 	en: {
 		page_title: " Coming soon · Johann Hospice ",
-		title: "Coming soon",
-		description_1: "I am Johann Hospice a junior developpeur based in france.",
-		description_2: "this web site is in developpement, it will serve as a support who permit to expose my cursus, skills and experiances.",
+		title: "Coming soon.",
+		description_1: "I am Johann Hospice a junior developer based in France.",
+		description_2: "This web site is in development, it will serve as a support who permit to expose my courses, skills and experiences.",
 		btn_info: "Learn more",
 		phone: "Phone"
 	},
 	fr: {
 		page_title: " Site en construction · Johann Hospice ",
-		title: "Site en construction",
+		title: "Site en construction.",
 		description_1: "Je m'appelle Johann Hospice et je suis développeur junior.",
 		description_2:"Ce site est en cours de développement, il servira de support permettant d'exposer mon cursus, mes compétences et mes expériences.",
 		btn_info: "En savoir plus",
