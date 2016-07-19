39f829be90abb29949903ed725865acab2c66f96
Johann <johannhospice.dev@gmail.com>
2016-07-19 08:23:29 +0200

    Update pages style

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 (function(){
-
+	(function(){
+		var $nav = $('nav');
+		setInterval(()=> $nav.toggleClass('nav-collapse'), 5000);
+	});
+	// (function(){ $('.item').css('height', ''+window.outerHeight) })()
+	(function () { setTimeout(()=>$('.link').addClass('link-active'), 1000)})()
 })(jQuery)
