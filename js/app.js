a487d1ee047a0bd18bd416099b97935f431a919f
Johann <johannhospice.dev@gmail.com>
2016-07-19 12:38:08 +0200

    Reduce animation time for intro

--- js/app.js
+++ js/app.js
@@ -4,5 +4,5 @@
 		setInterval(()=> $nav.toggleClass('nav-collapse'), 5000);
 	});
 	// (function(){ $('.item').css('height', ''+window.outerHeight) })()
-	(function () { setTimeout(()=>$('.link').addClass('link-active'), 1000)})()
+	(function () { setTimeout(()=>$('.link').addClass('link-active'), 500)})()
 })(jQuery)
