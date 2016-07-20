1ace2deb86802af0261182896c879309b2679b09
Johann <johannhospice.dev@gmail.com>
2016-07-20 13:10:36 +0200

    Clear js file

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 (function(){
-	(function(){
-		var $nav = $('nav');
-		setInterval(()=> $nav.toggleClass('nav-collapse'), 5000);
-	});
-	// (function(){ $('.item').css('height', ''+window.outerHeight) })()
-	(function () { setTimeout(()=>$('.link').addClass('link-active'), 500)})()
+	(function () { 
+		var $link = $('.link');
+		$link[0] && setTimeout(()=>$link.addClass('link-active'), 500)
+	})()
 })(jQuery)
