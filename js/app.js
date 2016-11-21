8face08485f30ee4098ae8dc479502192dff914d
johannhospice <johannhospice.dev@gmail.com>
2016-11-21 01:40:44 +0100

    Updating various files

--- js/app.js
+++ js/app.js
@@ -1,2 +1,8 @@
-(function(){
+(function () {
+	$('.collapse').collapse()
+    $('#tab-work a').click(function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    })
+    $('.carousel').carousel()
 })(jQuery)
