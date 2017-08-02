04caf241cb7e3e786a16996f427aec80131e3524
Johann Hospice <johannhospice.dev@gmail.com>
2017-08-02 12:54:47 +0200

    Adding images and updating work galerie layout

---
+++ public/works/script.js
@@ -0,0 +1,12 @@
+/*
+$(document).ready(function () {
+    $(".uk-card")
+        .hover(function (e) {
+            $(this).find("p.description")
+                .removeClass('uk-hidden');
+        }, function (e) {
+            $(this).find("p.description")
+                .addClass('uk-hidden');
+        })
+});
+    */
