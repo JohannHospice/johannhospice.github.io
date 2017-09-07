850ce26b94bf79c59d50b793dae2949108ca8d48
Johann Hospice <johannhospice.dev@gmail.com>
2017-09-08 01:32:27 +0200

    Assets update Arrangement of files Adding/Removing images (new svg logo...) Adding "me" css jss  && chart.js

---
+++ public/assets/vendors/me/me.js
@@ -0,0 +1,26 @@
+
+var ctx = document.getElementById("chart-language").getContext('2d');
+
+var myChart = new Chart(ctx, {
+    type: 'radar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 16, 10, 15],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+            ],
+            borderWidth: 1
+        }]
+    }
+});
