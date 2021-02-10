a5714a7b39207d9fb77cf591643fb3a417c15aec
Hospice <johannhospice.dev@gmail.com>
2021-02-10 20:23:56 +0100

    Updates

---
+++ assets/main.js
@@ -0,0 +1,36 @@
+var Translator = (function () {
+    const translator = new Map();
+    translator.set('a', '4');
+    translator.set('e', '3');
+    translator.set('i', '1');
+    translator.set('o', '0');
+    translator.set('s', '5');
+    translator.set('t', '7');
+    translator.set(' ', '_');
+    translator.set(',', '/');
+    return {
+        code: function (str) {
+            return str.toLowerCase().split('').map(function (c) {
+                return translator.has(c) ? translator.get(c) : c
+            }).join('').toUpperCase()
+        }
+    }
+})();
+
+document.querySelectorAll("[jh-code]").forEach(function (e) {
+    var text = e.getAttribute("jh-code")
+    var size = e.getAttribute("jh-size")
+
+    if (size == null)
+        size = 500
+
+    var code = Translator.code(text)
+    
+    var codeConcat = ""
+    var i = 0
+    while (codeConcat.length < size) {
+        codeConcat += code[i] + " "
+        i = (i + 1) % code.length
+    }
+    e.innerHTML = codeConcat
+})
